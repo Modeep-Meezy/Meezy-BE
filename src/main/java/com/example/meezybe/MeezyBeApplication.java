8c24b81b11cdac11631c3d0bd0b678bd2eb0a6cc
sorim0626 <enhld00@gmail.com>
2025-11-04 19:11:42 +0900

    settings

---
+++ src/main/java/com/example/meezybe/MeezyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.meezybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeezyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeezyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meezybe/MeezyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meezybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeezyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
